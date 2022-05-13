ef3f994fd99abebcb10d8a2b209c5163adf2fbf7
Long Nguyen <nmhoanglong@gmail.com>
2022-05-13 14:16:10 +0700

    add two files for searching from database

---
+++ 01_sql4.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+    """
+    # use a for loop to iterate through the database, printing the results line by line
+    for row in c.execute("SELECT firstname, lastname from employees"):
+        print(row)
+    """
+    c.execute("SELECT firstname, lastname from employees")
+    
+    rows= c.fetchall()
+    
+    for r in rows:
+        print(r[0], r[1])
